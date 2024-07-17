9f6c5db91ee666adc7784923a5d98dfec3106433
avanishpatidar <avanishpatidar555@gmail.com>
2024-07-17 20:07:32 +0530

    index.js

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,13 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// Configure CORS options
+const corsOptions = {
+  origin: '*', // Allow all origins
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Connect to MongoDB
